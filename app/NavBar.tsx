669d81860c9d9e8be3b471625a9f0b2ce10638ca
Touhid Hasan <touhidhsn@gmail.com>
2023-11-19 13:28:32 +0600

    added dropdown menu on user

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Box, Container, Flex } from '@radix-ui/themes';
+import {
+  Avatar,
+  Box,
+  Container,
+  DropdownMenu,
+  Flex,
+  Text,
+} from '@radix-ui/themes';
 import classNames from 'classnames';
 import { useSession } from 'next-auth/react';
 import Link from 'next/link';
@@ -43,7 +50,26 @@ const NavBar = () => {
           </Flex>
           <Box>
             {status === 'authenticated' && (
-              <Link href='/api/auth/signout'>Logout</Link>
+              <DropdownMenu.Root>
+                <DropdownMenu.Trigger>
+                  <Avatar
+                    src={session.user!.image!}
+                    fallback='?'
+                    size='2'
+                    radius='full'
+                    className='curson-pointer'
+                  />
+                </DropdownMenu.Trigger>
+                <DropdownMenu.Content>
+                  <DropdownMenu.Label>
+                    
+                    <Text size='2'>{session.user!.email}</Text>
+                  </DropdownMenu.Label>
+                  <DropdownMenu.Item>
+                    <Link href='/api/auth/signout'>Logout</Link>
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Root>
             )}
             {status === 'unauthenticated' && (
               <Link href='/api/auth/signin'>Log in</Link>
--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import { Button, Callout, Text, TextField } from '@radix-ui/themes';
-import dynamic from 'next/dynamic';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
