3a582298a12c8f7c0d8a1de29c7320536d5daa9e
Touhid Hasan <touhidhsn@gmail.com>
2023-11-21 22:07:35 +0600

    assign selector component created

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,7 @@ import IssueDetails from './IssueDetails';
 import DeleteIssueButtons from './DeleteIssueButtons';
 import { getServerSession } from 'next-auth';
 import authOptions from '@/app/auth/authOptions';
+import AssigneeSelect from './AssigneeSelect';
 
 interface Props {
   params: { id: string };
@@ -31,6 +32,7 @@ const IssueDetailPage = async ({ params }: Props) => {
       {session && (
         <Box>
           <Flex direction='column' gap='4'>
+            <AssigneeSelect />
             <EditIssueButton issueId={issue.id} />
             <DeleteIssueButtons issueId={issue.id} />
           </Flex>
